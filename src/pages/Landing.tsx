5ed8bdb03ca6a0594b80bdef8070fbc1a9203688
poseidon0z <adityaprabhu1290@gmail.com>
2025-09-06 10:48:04 +0530

    Slight fix for landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Descriptors from "../components/Landing/Descriptors";
-import Heading from "../components/General/Heading";
-import me from "../assets/Me.png";
+import React from 'react';
+import Descriptors from '../components/Landing/Descriptors';
+import Heading from '../components/General/Heading';
+import me from '../assets/Me.png';
 
 const Landing: React.FC = () => {
   return (
@@ -11,7 +11,7 @@ const Landing: React.FC = () => {
 
       <Descriptors />
 
-      <div className="grid grid-cols-12 w-screen min-h-[500px] grow px-8 pb-8 pt-16 gap-4">
+      <div className="grid grid-cols-12 w-screen min-h-[500px] grow px-8 pb-8 pt-16 gap-4 items-center">
         <div className="col-span-5 row-span-1 h-full flex flex-col pb-[20%]">
           <div className="text-[128px] font-heading font-bold text-white leading-none">
             Hi!
@@ -22,7 +22,7 @@ const Landing: React.FC = () => {
             I build software
           </Heading>
         </div>
-        <div className="col-start-9 col-span-4 flex flex-col items-center justify-end h-full min-h-0 pt-[20%]">
+        <div className="col-start-9 col-span-4 flex flex-col items-center justify-end h-4/5 min-h-0 pt-[20%]">
           <img src={me} className="h-full aspect-square object-contain" />
         </div>
       </div>
